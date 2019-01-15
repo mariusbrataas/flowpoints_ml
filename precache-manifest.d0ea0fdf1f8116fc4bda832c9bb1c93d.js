6de05005902e9b8f135c99203c2769d5572ad704
Marius Brataas <mariusbrataas@me.com>
2019-01-16 01:35:23 +0530

    Updates

---
+++ precache-manifest.d0ea0fdf1f8116fc4bda832c9bb1c93d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "7c0425a98b7923e51fe5",
+    "url": "/flowpoints_ml/static/js/runtime~main.7c0425a9.js"
+  },
+  {
+    "revision": "157cd18249c7654b0a71",
+    "url": "/flowpoints_ml/static/js/main.157cd182.chunk.js"
+  },
+  {
+    "revision": "8506a8de70392f6269ed",
+    "url": "/flowpoints_ml/static/js/1.8506a8de.chunk.js"
+  },
+  {
+    "revision": "157cd18249c7654b0a71",
+    "url": "/flowpoints_ml/static/css/main.4a8b45f7.chunk.css"
+  },
+  {
+    "revision": "4a14c86f99268301ca94911a0fd6eef1",
+    "url": "/flowpoints_ml/index.html"
+  }
+];

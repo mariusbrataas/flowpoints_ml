b7a11d18483796613f112e62bf39619165172e2b
Marius Brataas <mariusbrataas@me.com>
2019-02-26 03:32:53 +0100

    Updates

--- precache-manifest.307c72e60b2884a98954d82a898b6394.js
+++ precache-manifest.307c72e60b2884a98954d82a898b6394.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/flowpoints_ml/static/js/runtime~main.7c0425a9.js"
   },
   {
-    "revision": "a3ec3e716e0840e31b09",
-    "url": "/flowpoints_ml/static/js/main.a3ec3e71.chunk.js"
+    "revision": "e4e8d4bdb82c8b084bd2",
+    "url": "/flowpoints_ml/static/js/main.e4e8d4bd.chunk.js"
   },
   {
     "revision": "755061a162facc4404f5",
     "url": "/flowpoints_ml/static/js/1.755061a1.chunk.js"
   },
   {
-    "revision": "a3ec3e716e0840e31b09",
+    "revision": "e4e8d4bdb82c8b084bd2",
     "url": "/flowpoints_ml/static/css/main.7bd8e2e6.chunk.css"
   },
   {
-    "revision": "deac7413dff7752b7bc7f15e21fa057e",
+    "revision": "c9b761ff87d08536b19d66a731dd3172",
     "url": "/flowpoints_ml/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/flowpoints_ml/precache-manifest.1d30fb8ddc49b1f2551273fb141f9969.js"
+  "/flowpoints_ml/precache-manifest.307c72e60b2884a98954d82a898b6394.js"
 );
 
 workbox.clientsClaim();
